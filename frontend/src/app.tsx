9f61e47407dcef35969f8ba9fc4c3ff35c8b2e11
tygourm <thomasgourmelon.dev@gmail.com>
2025-08-21 19:59:02 +0200

    🎨 Add tailwindcss

---
+++ frontend/src/app.tsx
@@ -0,0 +1,12 @@
+import react from "/react.svg";
+
+import vite from "/vite.svg";
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <img src={vite} />
+      <img src={react} />
+    </div>
+  );
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,15 @@
 import react from "@vitejs/plugin-react-swc";
 
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
